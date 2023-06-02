ac5650c080b2086d01571091e437ff88daae282c
Neel Jepaliya <najepaliya@gmail.com>
2023-06-02 12:13:51 -0700

    add string value parsing

--- json_parser.hpp
+++ json_parser.hpp
@@ -9,9 +9,9 @@
 enum symbol : uint_fast8_t
 {
     // tokens
-    whitespace, n, f, t, digit, minus, dot, Ee, quote,
+    whitespace, n, f, t, digit, minus, dot, Ee, quote, character, backslash,
     // nonterminals
-    nt_whitespace, nt_value, nt_digits, nt_fraction, nt_exponent,
+    nt_whitespace, nt_value, nt_digits, nt_fraction, nt_exponent, nt_characters
 };
 
 void push_multiple (std::stack<uint_fast8_t>& stack, std::vector<uint_fast8_t>&& symbols)
@@ -46,6 +46,30 @@ bool is_digit (char& character)
     return result;
 }
 
+bool is_escape (char& character)
+{
+    bool result = false;
+    if (character == '"' || character == '\\' || character == '/' || character == 'b' || character == 'f' || character == 'n' || character == 'r' || character == 't')
+    {
+        result = true;
+    }
+    return result;
+}
+
+bool is_hex_pattern (std::string&& buffer)
+{
+    bool result = true;
+    for (uint_fast8_t index = 0; index < 4; index += 1)
+    {
+        if (is_digit (buffer[index]) || (buffer[index] > 64 && buffer[index] < 71) || (buffer[index] > 96 && buffer[index] < 103))
+        {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
 uint_fast8_t error ()
 {
     std::cout << "ERROR\n";
@@ -55,23 +79,23 @@ uint_fast8_t error ()
 uint_fast8_t parse_json (std::string&& buffer)
 {
     // repeating match: 3
-    uint8_t rule_table [5][9] =
+    uint8_t rule_table [6][11] =
     {
-    //  ws  n  f  t  #  -  .  Ee   "
-         3, 1, 1, 1, 1, 1, 1,  1,  0, // ws
-         0, 4, 5, 6, 7, 7, 0,  0, 10, // val
-         1, 1, 1, 1, 3, 1, 1,  1,  0, // digs
-         1, 0, 0, 0, 0, 0, 8,  1,  0, // frac
-         1, 0, 0, 0, 0, 0, 0,  9,  0, // exp
+    /*  ws  n  f  t  #  -  .  Ee   " ch   \ */
+         3, 1, 1, 1, 1, 1, 1,  1,  1, 1,  1, // ws
+         0, 4, 5, 6, 7, 7, 0,  0, 10, 0,  0, // val
+         1, 1, 1, 1, 3, 1, 1,  1,  1, 1,  1, // digs
+         1, 0, 0, 0, 0, 0, 8,  1,  0, 0,  0, // frac
+         1, 0, 0, 0, 0, 0, 0,  9,  0, 0,  0, // exp
+         3, 3, 3, 3, 3, 3, 3,  3,  2, 3, 11, // chs
     };
 
     std::stack<uint_fast8_t> stack;
     push_multiple(stack, {nt_whitespace, nt_value, nt_whitespace});
 
-    uint_fast8_t token;
-
     for (int index = 0; index < buffer.size(); index += 1)
     {
+        uint_fast8_t token = character;
         switch (buffer[index])
         {
             case ' ': case '\n': case '\r': case '\t':
@@ -97,10 +121,16 @@ uint_fast8_t parse_json (std::string&& buffer)
                 break;
             case 'E': case 'e':
                 token = Ee;
-                break;   
+                break;
+            case '"':
+                token = quote;
+                break;
+            case '\\':
+                token = backslash;
+                break;
         }
 
-        switch (rule_table[stack.top() % 9][token % 9])
+        switch (rule_table[stack.top() % 11][token % 11])
         {
             case 0: // error
                 return error ();
@@ -149,6 +179,7 @@ uint_fast8_t parse_json (std::string&& buffer)
                 }
                 return error ();
             case 9: // exponential number
+            {
                 uint_fast8_t jump = 1;
                 if (buffer[index + 1] == '+' || buffer[index + 1] == '-')
                 {
@@ -160,6 +191,26 @@ uint_fast8_t parse_json (std::string&& buffer)
                     push_multiple (stack, {nt_digits});
                     index += jump;
                     break;
+                }
+                return error ();
+            }
+            case 10: // string value
+                stack.pop();
+                push_multiple (stack, {nt_characters});
+                break;
+            case 11: // escape sequence
+                if (is_escape (buffer[index + 1]))
+                {
+                    index += 1;
+                    break;
+                }
+                else if (buffer[index + 1] == 'u')
+                {
+                    if (is_hex_pattern (buffer.substr(index + 2, 4)))
+                    {
+                        index += 5;
+                        break;
+                    }
                 }
                 return error ();
         }
