f25bdb55a8b285e8a8e9f0c85fb865b85a6e727f
Neel Jepaliya <najepaliya@gmail.com>
2023-06-07 14:50:04 -0700

    add type information

--- example.cpp
+++ example.cpp
@@ -1,17 +1,27 @@
 #include "json_parser.hpp"
 #include <iostream>
 
+// Integer associated types
+// Array   -2
+// Object  -1
+// Root     0
+// Null     1
+// False    2
+// True     3
+// Number   4
+// String   5
+
 int main()
 {
-    std::string s = "[0,1,2,3]";     // test string
-    json j;                          // create json object
-    j.parse (s);                     // parse and create index
-    token* t = j.index();            // get pointer to root of token tree
-    t = t->index({0,2});             // get pointer to specific token using index
-    std::cout << t->value() << "\n"; // print token value
-    t->set (4,"5");                  // set token to another value
-    std::cout << t->value() << "\n"; // print token value again
-    t = j.index();                   // get pointer to root of token tree
-    std::cout << t->value() << "\n"; // print all values from root
+    std::string s = "[{},null,false,true,0.1e+2,\"hello\"]";// test string
+    json j;                                                     // create json object
+    j.parse (s);                                                // parse and create index
+    token* t = j.index();                                       // get pointer to root of token tree
+    t = t->index({0,2});                                        // get pointer to specific token using index
+    std::cout << t->value() << "\n";                            // print token value
+    t->set (4,"5");                                             // set token to another value
+    std::cout << t->value() << "\n";                            // print token value again
+    t = j.index();                                              // get pointer to root of token tree
+    std::cout << t->value() << "\n";                            // print all values from root
     return 0;
 }
