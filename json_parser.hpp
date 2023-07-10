05f6ebebef6cb407b5b9d2e358ccee5d842fc953
Neel Jepaliya <najepaliya@gmail.com>
2023-07-10 14:27:51 -0700

    implement token class

--- json_parser.hpp
+++ json_parser.hpp
@@ -2,6 +2,25 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+
+class token
+{
+  public:
+    int8_t type;
+    std::string string;
+    std::vector<token> children;
+    token();
+    token (std::string s);
+};
+
+token::token()
+{
+}
+
+token::token (std::string s): string (std::move (s))
+{
+}
 
 class json
 {
