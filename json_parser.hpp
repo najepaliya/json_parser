032b1feea12f5b7ef1e53f95921b2aca0917b6a0
Neel Jepaliya <najepaliya@gmail.com>
2023-07-11 10:31:45 -0700

    add null, false and true value parsing

--- json_parser.hpp
+++ json_parser.hpp
@@ -53,9 +53,9 @@ void json::clear()
 enum symbol: uint_fast8_t
 {
 	// terminals
-	end, whitespace, quote, space, character, backslash, escape,
+	end, whitespace, n, u, l, f, a, s, e, t, r, // quote, space, character, backslash, escape, u, hex,
 	// nonterminals
-	nt_end, nt_primitive, nt_whitespace, nt_value, nt_string, nt_characters, nt_escape
+	nt_end, nt_primitive, nt_whitespace, nt_value, nt_u, nt_l, nt_a, nt_s, nt_e, nt_r // nt_string, nt_characters, nt_escape, nt_hex
 };
 
 void json::parse (std::string& buffer)
@@ -66,16 +66,19 @@ void json::parse (std::string& buffer)
 	// append null terminator
 	buffer.push_back ('\0');
 
-	uint_fast8_t rule_table [7][7] =
+	uint_fast8_t rule_table [10][11] =
 	{
-/*  $ ws  " sp ch  \ esc  */
-		4, 0, 0, 0, 0, 0,  0, // end  0
-		7, 7, 7, 7, 7, 7,  0, // prim 1 FIX LATER
-		1, 3, 1, 3, 0, 0,  0, // ws   2
-		0, 5, 5, 0, 0, 0,  0, // val  3
-		0, 0, 6, 0, 0, 0,  0, // str  4
-		0, 0, 2, 3, 3, 8,  3, // chrs 5
-		0, 0, 2, 0, 0, 2,  2, // esc  6
+/*  $ ws  n  u  l  f  a  s  e  t  r  */
+		4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // end  0
+		5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, // prim 1
+		1, 3, 1, 0, 0, 1, 0, 0, 0, 1, 0, // ws   2
+		0, 0, 6, 0, 0, 7, 0, 0, 0, 8, 0, // val  3
+		0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, // u    4
+		0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, // l    5
+		0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, // a    6
+		0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, // s    7
+		0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, // e    8
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, // r    9
 	};
 	
 	// initialize symbol stack
@@ -94,31 +97,64 @@ void json::parse (std::string& buffer)
 	
 	for (int index = 0; ; index += 1)
 	{
-		uint_fast8_t terminal = character;
+		uint_fast8_t terminal; //= character;
 
 		switch (buffer[index])
 		{
 			case '\0':
 				terminal = end;
 				break;
-			case '\n': case '\r': case '\t':
+			case ' ': case '\n': case '\r': case '\t':
 				terminal = whitespace;
 				break;
-			case '"':
-				terminal = quote;
+			case 'n':
+				terminal = n;
 				break;
-			case ' ':
-				terminal = space;
+			case 'u':
+				terminal = u;
 				break;
-			case '\\':
-				terminal = backslash;
+			case 'l':
+				terminal = l;
 				break;
-			case '/': case 'b': case 'f': case 'n': case 'r': case 't':
-				terminal = escape;
+			case 'f':
+				terminal = f;
 				break;
+			case 'a':
+				terminal = a;
+				break;
+			case 's':
+				terminal = s;
+				break;
+			case 'e':
+				terminal = e;
+				break;
+			case 't':
+				terminal = t;
+				break;
+			case 'r':
+				terminal = r;
+				break;
+			// case '"':
+			// 	terminal = quote;
+			// 	break;
+			// case ' ':
+			// 	terminal = space;
+			// 	break;
+			// case '\\':
+			// 	terminal = backslash;
+			// 	break;
+			// case '/': case 'b': case 'f': case 'n': case 'r': case 't':
+			// 	terminal = escape;
+			// 	break;
+			// case 'u':
+			// 	terminal = u;
+			// 	break;
+			// case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case 'a': case 'c': case 'd': case 'e': case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+			// 	terminal = hex;
+			// 	break;
 		}
 
-		switch (rule_table[symbols.top() % 7][terminal % 7])
+		switch (rule_table[symbols.top() % 11][terminal % 11])
 		{
 			case 0: // error
 				std::cout << "==ERROR: " << index << "->'" << buffer[index] << "'==\n";
@@ -137,26 +173,55 @@ void json::parse (std::string& buffer)
 				std::cout << "==VALID==\n";
 				buffer.pop_back();
 				return;
-			case 5: // string value
-				symbols.pop();
-				symbols.push (nt_string);
-				index -= 1;
-				break;
-			case 6: // decompose string
-				symbols.pop();
-				indexes.push (index);
-				symbols.push (nt_primitive);
-				symbols.push (nt_characters);
-				break;
-			case 7: // store primitive
+			case 5: // store primitive
 				containers.top()->children.emplace_back (buffer.substr (indexes.top(), index - indexes.top()));
 				indexes.pop();
 				symbols.pop();
 				index -= 1;
 				break;
-			case 8:
-				symbols.push (nt_escape);
+			case 6: // null value
+				symbols.pop();
+				// symbols.push (nt_primitive);
+				symbols.push (nt_l);
+				symbols.push (nt_l);
+				symbols.push (nt_u);
 				break;
+			case 7: // null value
+				symbols.pop();
+				// symbols.push (nt_primitive);
+				symbols.push (nt_e);
+				symbols.push (nt_s);
+				symbols.push (nt_l);
+				symbols.push (nt_a);
+				break;
+			case 8: // null value
+				symbols.pop();
+				// symbols.push (nt_primitive);
+				symbols.push (nt_e);
+				symbols.push (nt_u);
+				symbols.push (nt_r);
+				break;
+			// case 5: // string value
+			// 	symbols.pop();
+			// 	symbols.push (nt_string);
+			// 	index -= 1;
+			// 	break;
+			// case 6: // decompose string
+			// 	symbols.pop();
+			// 	indexes.push (index);
+			// 	symbols.push (nt_primitive);
+			// 	symbols.push (nt_characters);
+			// 	break;
+			
+			// case 8:
+			// 	symbols.push (nt_escape);
+			// 	break;
+			// case 9:
+			// 	symbols.push (nt_hex);
+			// 	symbols.push (nt_hex);
+			// 	symbols.push (nt_hex);
+			// 	symbols.push (nt_hex);
+			// 	break;
 		}
 	}
 }
