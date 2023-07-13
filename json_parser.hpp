5ea147dd09529ea1f98bd0773674592c70499d06
Neel Jepaliya <najepaliya@gmail.com>
2023-07-12 17:38:13 -0700

    fix number parsing and add unicode parsing

--- json_parser.hpp
+++ json_parser.hpp
@@ -53,9 +53,9 @@ void json::clear()
 enum symbol: uint_fast8_t
 {
 	// terminals
-	end, whitespace, n, u, l, f, a, s, e, t, r, digit, dot, E, sign, quote, space, character, backslash, slash, b,
+	end, whitespace, n, u, l, f, a, s, e, t, r, minus, zero, onenine, dot, E, plus, quote, space, character, backslash, slash, b, hex,
 	// nonterminals
-	nt_end, nt_primitive, nt_whitespace, nt_value, nt_u, nt_l, nt_a, nt_s, nt_e, nt_r, nt_integer, nt_digit, nt_fraction, nt_sign, nt_exponent, nt_string, nt_characters, nt_escape // nt_string, nt_characters, nt_escape, nt_hex
+	nt_end, nt_primitive, nt_whitespace, nt_value, nt_u, nt_l, nt_a, nt_s, nt_e, nt_r, nt_minus, nt_integer, nt_digits, nt_fraction, nt_digit, nt_sign, nt_exponent, nt_string, nt_characters, nt_escape, nt_hex
 };
 
 void json::parse (std::string& buffer)
@@ -66,27 +66,30 @@ void json::parse (std::string& buffer)
 	// append null terminator
 	buffer.push_back ('\0');
 
-	uint_fast8_t rule_table [18][21] =
+	uint_fast8_t rule_table [21][24] =
 	{
-/*  $ ws  n  u  l  f  a  s   e  t  r  #   .   E +-   "  sp  ch   \  /  b  */
-		4, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // end   0
-		5, 5, 5, 5, 5, 5, 5, 5,  5, 5, 5, 5,  5,  5, 5,  5,  5,  5,  0, 0, 0, // prim  1
-		1, 3, 1, 0, 0, 1, 0, 0,  0, 1, 0, 1,  0,  0, 0,  1,  3,  0,  0, 0, 0, // ws    2
-		0, 0, 6, 0, 0, 7, 0, 0,  0, 8, 0, 9,  0,  0, 0, 12,  0,  0,  0, 0, 0, // val   3
-		0, 0, 0, 2, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // u     4
-		0, 0, 0, 0, 2, 0, 0, 0,  0, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // l     5
-		0, 0, 0, 0, 0, 0, 2, 0,  0, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // a     6
-		0, 0, 0, 0, 0, 0, 0, 2,  0, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // s     7
-		0, 0, 0, 0, 0, 0, 0, 0,  2, 0, 0, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // e     8
-		0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 2, 0,  0,  0, 0,  0,  0,  0,  0, 0, 0, // r     9
-		1, 1, 0, 0, 0, 0, 0, 0, 11, 0, 0, 3, 10, 11, 0,  0,  1,  0,  0, 0, 0, // int  10
-		0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 2,  0,  0, 0,  0,  0,  0,  0, 0, 0, // dig  11
-		1, 1, 0, 0, 0, 0, 0, 0, 11, 0, 0, 3,  0, 11, 0,  0,  1,  0,  0, 0, 0, // frac 12
-		0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 1,  0,  0, 2,  0,  0,  0,  0, 0, 0, // sign 13
-		1, 1, 0, 0, 0, 0, 0, 0,  0, 0, 0, 3,  0,  0, 0,  0,  1,  0,  0, 0, 0, // exp  14
-		0, 0, 0, 0, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0, 0, 13,  0,  0,  0, 0, 0, // str  15
-		0, 0, 3, 3, 3, 3, 3, 3,  3, 3, 3, 3,  3,  3, 3,  2,  3,  3, 14, 0, 0, // chrs 16
-		0, 0, 2, 0, 0, 2, 0, 0,  0, 2, 2, 0,  0,  0, 0,  2,  0,  0,  2, 2, 2, // esc  17
+/*  $ ws  n   u  l  f  a  s   e  t  r  -   0  19   .   E  +   "  sp  ch   \  /  b hx  */
+		4, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // end   0
+		5, 5, 5,  5, 5, 5, 5, 5,  5, 5, 5, 5,  5,  5,  5,  5, 5,  0,  5,  5,  5, 5, 5, 0, // prim  1
+		1, 3, 1,  0, 0, 1, 0, 0,  0, 1, 0, 1,  1,  1,  0,  0, 0,  1,  3,  0,  0, 0, 0, 0, // ws    2
+		0, 0, 6,  0, 0, 7, 0, 0,  0, 8, 0, 9,  9,  9,  0,  0, 0, 13,  0,  0,  0, 0, 0, 0, // val   3
+		0, 0, 0,  2, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // u     4
+		0, 0, 0,  0, 2, 0, 0, 0,  0, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // l     5
+		0, 0, 0,  0, 0, 0, 2, 0,  0, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // a     6
+		0, 0, 0,  0, 0, 0, 0, 2,  0, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // s     7
+		0, 0, 0,  0, 0, 0, 0, 0,  2, 0, 0, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // e     8
+		0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 2, 0,  0,  0,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // r     9
+		0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 2,  1,  1,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // min  10
+		0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0,  2, 10,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // int  11
+		1, 1, 0,  0, 0, 0, 0, 0,  1, 0, 0, 0,  3,  3,  1,  1, 0,  0,  1,  0,  0, 0, 0, 0, // digs 12
+		0, 1, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0, 11,  0, 0,  0,  0,  0,  0, 0, 0, 0, // frc  13
+		0, 0, 0,  0, 0, 0, 0, 0,  2, 0, 0, 0,  2,  2,  0,  0, 0,  0,  0,  0,  0, 0, 0, 0, // dig  14
+		0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 2,  1,  1,  0,  0, 2,  0,  0,  0,  0, 0, 0, 0, // sig  15
+		1, 1, 0,  0, 0, 0, 0, 0, 12, 0, 0, 0,  0,  0,  0, 12, 0,  0,  0,  0,  0, 0, 0, 0, // exp  16
+		0, 0, 0,  0, 0, 0, 0, 0,  0, 0, 0, 0,  0,  0,  0,  0, 0, 14,  0,  0,  0, 0, 0, 0, // str  17
+		0, 0, 3,  3, 3, 3, 3, 3,  3, 3, 3, 3,  3,  3,  3,  3, 3,  2,  3,  3, 15, 3, 3, 3, // chrs 18
+		0, 0, 2, 16, 0, 2, 0, 0,  0, 2, 2, 0,  0,  0,  0,  0, 0,  2,  0,  0,  2, 2, 2, 0, // esc  19
+		0, 0, 0,  0, 0, 2, 2, 0,  2, 0, 0, 0,  2,  2,  0,  2, 0,  0,  0,  0,  0, 0, 2, 2, // hex  20
 	};
 	
 	// initialize symbol stack
@@ -142,8 +145,14 @@ void json::parse (std::string& buffer)
 			case 'r':
 				terminal = r;
 				break;
-			case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-				terminal = digit;
+			case '-':
+				terminal = minus;
+				break;
+			case '0':
+				terminal = zero;
+				break;
+			case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+				terminal = onenine;
 				break;
 			case '.':
 				terminal = dot;
@@ -151,8 +160,8 @@ void json::parse (std::string& buffer)
 			case 'E':
 				terminal = E;
 				break;
-			case '+': case '-':
-				terminal = sign;
+			case '+':
+				terminal = plus;
 				break;
 			case '"':
 				terminal = quote;
@@ -169,27 +178,16 @@ void json::parse (std::string& buffer)
 			case 'b':
 				terminal = b;
 				break;
-			// case '"':
-			// 	terminal = quote;
-			// 	break;
-			// case ' ':
-			// 	terminal = space;
-			// 	break;
-			
-			// case 'u':
-			// 	terminal = u;
-			// 	break;
-			// case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case 'a': case 'c': case 'd': case 'e': case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-			// 	terminal = hex;
-			// 	break;
+			case 'A': case 'B': case 'C': case 'D': case 'F': case 'c': case 'd':
+				terminal = hex;
+				break;
 		}
 
-		switch (rule_table[symbols.top() % 21][terminal % 21])
+		switch (rule_table[symbols.top() % 24][terminal % 24])
 		{
 			case 0: // error
 				std::cout << buffer << "\n";
-				std::cout << "t: '" << buffer[index] << "' i: " << index << " r: " << symbols.top() % 18 << "\n";
-				// std::cout << "==ERROR: " << index << "->'" << buffer[index] << "'==\n";
+				std::cout << "t: '" << buffer[index] << "' i: " << index << " r: " << symbols.top() % 24 << "\n";
 				clear();
 				buffer.pop_back();
 				return;
@@ -240,54 +238,48 @@ void json::parse (std::string& buffer)
 				symbols.pop();
 				indexes.push (index);
 				symbols.push (nt_primitive);
-				symbols.push (nt_integer);
-				break;
-			case 10: // fractional number
-				symbols.pop();
+				symbols.push (nt_exponent);
 				symbols.push (nt_fraction);
+				symbols.push (nt_integer);
+				symbols.push (nt_minus);
+				index -= 1;
+				break;
+			case 10: // continue int
+				symbols.pop();
+				symbols.push (nt_digits);
+				break;
+			case 11: // fractional number
+				symbols.pop();
+				symbols.push (nt_digits);
 				symbols.push (nt_digit);
 				break;
-			case 11: // exponential value
+			case 12: // exponential number
 				symbols.pop();
-				symbols.push (nt_exponent);
+				symbols.push (nt_digits);
 				symbols.push (nt_digit);
 				symbols.push (nt_sign);
 				break;
-			case 12: // string value
+			case 13: // string value
 				symbols.pop();
 				symbols.push (nt_string);
 				index -= 1;
 				break;
-			case 13: // decompose string
+			case 14: // decompose string
 				symbols.pop();
 				indexes.push (index);
 				symbols.push (nt_primitive);
 				symbols.push (nt_characters);
 				break;
-			case 14: // escape sequence
+			case 15: // escape sequence
 				symbols.push (nt_escape);
 				break;
-			// case 5: // string value
-			// 	symbols.pop();
-			// 	symbols.push (nt_string);
-			// 	index -= 1;
-			// 	break;
-			// case 6: // decompose string
-			// 	symbols.pop();
-			// 	indexes.push (index);
-			// 	symbols.push (nt_primitive);
-			// 	symbols.push (nt_characters);
-			// 	break;
-			
-			// case 8:
-			// 	symbols.push (nt_escape);
-			// 	break;
-			// case 9:
-			// 	symbols.push (nt_hex);
-			// 	symbols.push (nt_hex);
-			// 	symbols.push (nt_hex);
-			// 	symbols.push (nt_hex);
-			// 	break;
+			case 16: // unicode sequence
+				symbols.pop();
+				symbols.push (nt_hex);
+				symbols.push (nt_hex);
+				symbols.push (nt_hex);
+				symbols.push (nt_hex);
+				break;
 		}
 	}
 }
